063ae7956543d715e850581e66159f1937295a00
Arkadiusz Tetloch <a.tetloch@sohbi-poland.com>
2026-07-03 08:26:23 +0200

    chore: przeniesienie konfiguracji Supabase do gitignore i dodanie szablonu

--- lib/core/supabase_client.dart
+++
@@ -1,4 +0,0 @@
-class SupabaseConfig {
-  static const String url = 'https://ldshnzwxvwvbsxjvvoht.supabase.co';
-  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkc2huend4dnd2YnN4anZ2b2h0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNTIwMjMsImV4cCI6MjA4MzYyODAyM30.uDJrLQVyape7cUwbzAasP5VMYfY-eHvNQaLl2Sr5pSw';
-}
